5891245f761540694a4f8387c20cae4f17bbd5a3
Meiko AKITA <meiko.akita@6code.jp>
2018-02-18 00:09:46 +0900

    起動時にCSVファイルを読み込む。

--- src/main/java/jp/hexachord/api/dto/Rate.java
+++ src/main/java/jp/hexachord/api/dto/Rate.java
@@ -2,10 +2,27 @@ package jp.hexachord.api.dto;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 @Data
 public class Rate {
 	private String dateStr;
 	private BigDecimal usd;
+
+	@JsonIgnore
+	private int year;
+	@JsonIgnore
+	private int month;
+	@JsonIgnore
+	private int day;
+
+	public void setDateStr(String dateStr) {
+		this.dateStr = dateStr;
+		String[] date = dateStr.split("/");
+		this.year = Integer.valueOf(date[0]);
+		this.month = Integer.valueOf(date[1]);
+		this.day = Integer.valueOf(date[2]);
+	}
 }
--- src/main/java/jp/hexachord/api/service/RateDataService.java
+++ src/main/java/jp/hexachord/api/service/RateDataService.java
@@ -1,12 +1,19 @@
 package jp.hexachord.api.service;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import jp.hexachord.api.dto.Rate;
@@ -14,27 +21,39 @@ import jp.hexachord.api.dto.Rate;
 @Component
 public class RateDataService {
 
-	private static List<Rate> data;
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	private static final String csvFilePath = "data/market_quote.csv";
+	private static List<String[]> csvdata = new ArrayList<>();;
 
 	@PostConstruct
 	public void initAfterStartup() {
-		// TODO: CSVファイル読み込み
-		data = Arrays.asList(
-				new Rate() {
-					{
-						setDateStr("2018/2/5");
-						setUsd(new BigDecimal("109.9"));
-					}
-				},
-				new Rate() {
-					{
-						setDateStr("2018/2/6");
-						setUsd(new BigDecimal("109.08"));
-					}
-				});
+		// CSVファイル読み込み
+		Resource resource = resourceLoader.getResource("classpath:" + csvFilePath);
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(resource.getFile()), "Shift-JIS"))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				csvdata.add(line.split(","));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public List<Rate> getRateData(Map<String, String> queryParameters) {
-		return data;
+		return csvdata.stream()
+				.skip(3) // 先頭3行のヘッダーをスキップ
+				.map(r -> new Rate() {
+					{
+						setDateStr(r[0]);
+						setUsd(new BigDecimal(r[1]));
+					}
+				})
+				//				.filter(r -> r.getDateStr().equals("2018/2/7")) //TODO:
+				.collect(Collectors.toList());
 	}
 }
