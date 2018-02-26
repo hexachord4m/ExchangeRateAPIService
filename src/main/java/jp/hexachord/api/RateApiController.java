08cd60be63ca170c52fa19b69ddce5912469077a
mayakt <mayakt@192.168.1.5>
2018-02-26 23:12:17 +0900

    起動時とAPI呼び出し時にCSVファイルをダウンロードして置き換える処理を追加。

--- src/main/java/jp/hexachord/api/RateApiController.java
+++ src/main/java/jp/hexachord/api/RateApiController.java
@@ -3,6 +3,7 @@ package jp.hexachord.api;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +17,16 @@ import jp.hexachord.api.service.RateDataService;
 @RequestMapping("/api")
 public class RateApiController {
 
+	@Autowired
+	RateDataService rateDataService;
+
 	@RequestMapping(value="/rate", method=RequestMethod.GET)
 	public List<Rate> getRateData(@RequestParam Map<String, String> queryParameters) {
-		return new RateDataService().getRateData(queryParameters);
+		return rateDataService.getRateData(queryParameters);
+	}
+
+	@RequestMapping(value="/rate/update")
+	public void refreshUpdateData() {
+		rateDataService.downloadCsvFile();
 	}
 }
--- src/main/java/jp/hexachord/api/service/RateDataService.java
+++ src/main/java/jp/hexachord/api/service/RateDataService.java
@@ -1,8 +1,15 @@
 package jp.hexachord.api.service;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -24,10 +31,14 @@ public class RateDataService {
 	private ResourceLoader resourceLoader;
 
 	private static final String csvFilePath = "data/market_quote.csv";
+	private static final String csvFileUrl = "https://www.mizuhobank.co.jp/rate/market/csv/quote.csv";
 	private static List<String[]> csvdata = new ArrayList<>();;
 
 	@PostConstruct
 	public void initAfterStartup() {
+		// CSVファイルダウンロード
+		downloadCsvFile();
+
 		// CSVファイル読み込み
 		Resource resource = resourceLoader.getResource("classpath:" + csvFilePath);
 		try (BufferedReader reader = new BufferedReader(
@@ -47,5 +58,49 @@ public class RateDataService {
 		CalcType calcType = CalcType.getCalcType(queryParameters.get("calc"));
 		IRateDataCreator rateDataCreator = RateDataCreatorFactory.GetRateDataCreator(calcType);
 		return rateDataCreator.getRateData(csvdata, queryParameters);
+	}
+
+	public void downloadCsvFile() {
+		HttpURLConnection conn = null;
+		DataInputStream dataInStream = null;
+		DataOutputStream dataOutStream = null;
+		// HttpURLConnectionはAutoCloseableを実装していないので、try-with-resourcesが使えずレガシーな実装に
+		try {
+			URL url = new URL(csvFileUrl);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.connect();
+
+			int statusCode = conn.getResponseCode();
+			if(statusCode != HttpURLConnection.HTTP_OK) {
+				throw new Exception("statusCode is " + statusCode);
+			}
+
+			dataInStream = new DataInputStream(conn.getInputStream());
+			Resource resource = resourceLoader.getResource("classpath:" + csvFilePath);
+			dataOutStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(resource.getFile())));
+
+			byte[] b = new byte[4096];
+			int readByte = 0;
+			while(-1 != (readByte = dataInStream.read(b))){
+				dataOutStream.write(b, 0, readByte);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(dataInStream != null) {
+					dataInStream.close();
+				}
+				if(dataOutStream != null) {
+					dataOutStream.close();
+				}
+				if(conn != null) {
+					conn.disconnect();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
