b2d36b68f64ea8987f09cba18e8c322c99114659
Meiko AKITA <meiko.akita@6code.jp>
2018-03-04 23:48:38 +0900

    平均実装中。

--- src/main/java/jp/hexachord/api/dto/Rate.java
+++ src/main/java/jp/hexachord/api/dto/Rate.java
@@ -23,14 +23,17 @@ public class Rate {
 	public void setDateOrg(String date) {
 		this.date = date;
 		String[] dateAry = date.split("/");
-		this.year = Integer.valueOf(dateAry[0]);
-		this.month = Integer.valueOf(dateAry[1]);
-		this.day = Integer.valueOf(dateAry[2]);
+		if (dateAry.length > 0)
+			this.year = Integer.valueOf(dateAry[0]);
+		if (dateAry.length > 1)
+			this.month = Integer.valueOf(dateAry[1]);
+		if (dateAry.length > 2)
+			this.day = Integer.valueOf(dateAry[2]);
 		this.dateNum = Integer.parseInt(String.format("%d%02d%02d", this.year, this.month, this.day));
 	}
 
-	//	@JsonIgnore
-	//	public String getYearMonthStr() {
-	//		return String.format("%d%02d", year, month);
-	//	}
+	@JsonIgnore
+	public String getYearMonthStr() {
+		return String.format("%d/%d", year, month);
+	}
 }
--- src/main/java/jp/hexachord/api/service/AverageRateDataCreator.java
+++ src/main/java/jp/hexachord/api/service/AverageRateDataCreator.java
@@ -1,7 +1,12 @@
 package jp.hexachord.api.service;
 
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import jp.hexachord.api.dto.Rate;
 
@@ -9,22 +14,41 @@ public class AverageRateDataCreator implements IRateDataCreator {
 
 	@Override
 	public List<Rate> getRateData(List<String[]> csvdata, Map<String, String> queryParameters) {
-		//		return csvdata.stream()
-		//				.skip(3) // 先頭3行のヘッダーをスキップ
-		//				.map(r -> new Rate() {
-		//					{
-		//						setDateStr(r[0]);
-		//						setUsd(new BigDecimal(r[1]));
-		//					}
-		//				})
-		//				.filter(r ->  r.getYear() >= startYear)
-		//				.filter(r ->  r.getYear() <= endYear)
-		//				.collect(Collectors.groupingBy(r -> r.getYearMonthStr(),
-		//						Collectors.averagingDouble(r -> r.getUsd().doubleValue()))) // doubleに変換して平均を算出
-		//				.entrySet().stream() // グループ化の結果をソートするためにstreamを再取得
-		//				.sorted(Map.Entry.comparingByKey()); // キーの昇順でソート
-		//				.collect(Collectors.toList());
-		return null; //TODO
-	}
+		Stream<Rate> stream = csvdata.stream()
+				.skip(3) // 先頭3行のヘッダーをスキップ
+				.map(r -> new Rate() {
+					{
+						setDateOrg(r[0]);
+						setUsd(new BigDecimal(r[1]));
+					}
+				});
 
+		if (queryParameters.containsKey("date")) {
+			int date = queryParameters.get("date").toLowerCase().equals("now")
+					? Integer.valueOf(new SimpleDateFormat("yyyyMMdd").format(new Date()))
+					: Integer.valueOf(queryParameters.get("date"));
+			stream = stream.filter(r -> r.getDateNum() == date);
+		} else {
+			if (queryParameters.containsKey("startdate")) {
+				stream = stream.filter(r -> r.getDateNum() >= Integer.valueOf(queryParameters.get("startdate")));
+			}
+			if (queryParameters.containsKey("enddate")) {
+				stream = stream.filter(r -> r.getDateNum() <= Integer.valueOf(queryParameters.get("enddate")));
+			}
+		}
+
+		return stream.collect(Collectors.groupingBy(r -> r.getYearMonthStr(),
+				Collectors.averagingDouble(r -> r.getUsd().doubleValue()))) // doubleに変換して平均を算出
+				.entrySet().stream() // グループ化の結果をソートするためにstreamを再取得
+				.sorted(Map.Entry.comparingByKey()) // キーの昇順でソート
+				.map(r -> new Rate() {
+					{
+						setDate(r.getKey());
+						BigDecimal bd = new BigDecimal(r.getValue().toString());
+						BigDecimal bd1 = bd.setScale(2, BigDecimal.ROUND_HALF_UP);
+						setUsd(bd1);
+					}
+				})
+				.collect(Collectors.toList());
+	}
 }
